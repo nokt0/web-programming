77de5ae70d27ea711be1bc6861b3d007220bde29
vladislav.snimshchikov <vlad60870@gmail.com>
2021-03-08 14:21:05 +0300

    HeaderMenu layout fix

--- src/components/FavoriteCities/favoriteCities.data.js
+++ src/components/FavoriteCities/favoriteCities.data.js
@@ -9,5 +9,16 @@ export const favoriteCitiesData = [
             {title: 'Влажность', value: '52%'},
             {title: 'Координаты', value: '[59.88, 30.42]'}
         ]
+    },
+    {
+        city: 'Helsinki',
+        degree: 3,
+        weatherDetails: [
+            {title: 'Ветер', value: 'Moderate breeze, 6.0 m/s, North-northwest'},
+            {title: 'Облачность', value: 'Broken clouds'},
+            {title: 'Давление', value: '1013 hpa'},
+            {title: 'Влажность', value: '52%'},
+            {title: 'Координаты', value: '[59.88, 30.42]'}
+        ]
     }
 ];
