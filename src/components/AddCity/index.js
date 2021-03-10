b5f927cbb2f5f9325cc8c1469cb2ce1dffe1738b
vladislav.snimshchikov <vlad60870@gmail.com>
2021-03-10 18:38:42 +0300

    use round button from component

--- src/components/AddCity/index.js
+++ src/components/AddCity/index.js
@@ -1,2 +1,11 @@
 import './addCity.scss';
+import {createRoundButton} from "../RoundButton";
 export AddCity from './addCity.html';
+
+const cityInputContainerId = 'add-city-input-container';
+
+export function initializeAddCity(){
+    const inputContainer = document.getElementById(cityInputContainerId);
+    const createdButton = createRoundButton('+');
+    inputContainer.appendChild(createdButton);
+}
--- src/containers/FavoriteCities/index.js
+++ src/containers/FavoriteCities/index.js
@@ -1,12 +1,13 @@
 import './favoriteCities.scss';
 import {favoriteCitiesData} from './favoriteCities.data';
-import {AddCity} from '../../components/AddCity'
+import {AddCity, initializeAddCity} from '../../components/AddCity'
 import {createCityCard} from "../../components/CityCard";
 export FavoriteCities from './favoriteCities.html';
 
 
 export function initializeFavoriteCities(){
     document.getElementById('favorite-cities-add').innerHTML = AddCity;
+    initializeAddCity();
     const cities = document.getElementById('favorite-cities-cities');
     const cards = favoriteCitiesData.map((data,index) => createCityCard(index + 1, data));
     cards.forEach((card)=>{cities.appendChild(card)});
